6ce086998c59105e0a1809c1d39ee9a16b84c473
RIchard Santana <121190316+Drahxa@users.noreply.github.com>
2023-11-06 12:17:07 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args)
     {
+        Cow franny = new Cow();
+        Cow gretta = new Cow("Gretta", 100, 1, false);
+        
+        Chicken clucky = new Chicken();
+        Chicken marshmallow = new Chicken("Sussex", 83.5, 2, false);
+        
+        Pig piglet = new Pig();
+        Pig pickles = new Pig("Yellow", 567.88, 10, true);
+
+        franny.makeNoise();
+        clucky.makeNoise();
+        pickles.makeNoise();
+        gretta.haveBirthday();
+        marshmallow.eatFood(10);
+        piglet.upgradePenSize(3);
 
     }
 
