3ad56828a2004777d134c1d9492058c39a7e0d03
RIchard Santana <121190316+Drahxa@users.noreply.github.com>
2023-11-06 12:15:50 -0500

    Update Chicken.java

--- src/Chicken.java
+++ src/Chicken.java
@@ -1,2 +1,38 @@
 public class Chicken {
+
+    //INSTANCE VARIABLE
+    private String breed;
+    private double weight;
+    private int egg;
+    private boolean mean;
+
+
+
+    //CONTRUCTORS
+
+    public Chicken(){
+        this.breed = "Unknown";
+        this.weight = 0;
+        this.egg = 0;
+        this.mean = false;
+    }
+    public Chicken(String breed, double weight, int egg, boolean mean){
+        this.breed = breed;
+        this.weight = weight;
+        this.egg = egg;
+        this.mean = mean;
+    }
+
+
+    //METHODS
+    public void makeNoise(){
+        System.out.println("CocoroCO");
+    }
+    public void eatFood(int amount){
+        this.weight += amount;
+    }
+    public void displayChicken(){
+        System.out.println("Breed:" + breed + "\nWeight:" + weight + "\nEggs:" + egg + "\nMean:" + mean);
+    }
+
 }
